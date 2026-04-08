d22b256a146a79f1fea1ec88a35149982e3429d2
msherin-clt <msherin@charlotte.edu>
2026-04-08 16:30:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
 // Middleware
 app.use(cors()); // Allow cross-origin requests
